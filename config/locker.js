5c1a675ba4fee1cbe2c276235b297229a8e1c826
mkhizeryounas <m.khizeryounas@gmail.com>
2018-03-16 06:50:44 +0500

    OK

--- config/locker.js
+++ config/locker.js
@@ -6,33 +6,20 @@ let data = {
     unlock: (request, response, next) => {
         let authHeader = request.headers['authorization'] || "";
         if(typeof authHeader !== "undefined") {
-            try {
-                authHeader = authHeader.split(" ");
-                if(authHeader[0].toLowerCase() !== "bearer") throw "Token type Bearer required";
-                authHeader = authHeader[1];
-                 jwt.verify(authHeader, cert, (err, decode) => {
-                    try {
-                        if(err) throw err;
-                        request.user = decode;
-                        next();
-                    }
-                    catch (error) {
-                        response.json({
-                            status: false,
-                            message: "JWT error occoured",
-                            data: error
-                        });
-                        return false;
-                    }
-                });
-            }
-            catch (error) {
-                response.json({
-                    status: false,
-                    message: error,
-                });
-                return false;
-            }
+            jwt.verify(authHeader, cert, (err, decode) => {
+                try {
+                    if(err) throw err;
+                    request.user = decode;
+                    next();
+                }
+                catch (error) {
+                    response.json({
+                        status: false,
+                        message: "JWT error occoured",
+                        data: error
+                    });
+                }
+            });
         }
     },
     lock: (obj) => {
